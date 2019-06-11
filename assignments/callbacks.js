9f2cc9388f5ccdc83d8cdb9f771d517aff41940f
Kevin Tou <toukevin@gmail.com>
2019-06-11 12:28:24 -0700

    Completed MVP and stretch

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,6 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
 
 /* 
 
@@ -24,27 +24,56 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
-
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, function(length) {
+  console.log("The items array length is: ", length);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+
+last(items, function(item) {
+  console.log("The last item is: ", item);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  const sum = x + y;
+  return cb(sum);
 }
+
+sumNums(2, 4, function(sumNum) {
+  console.log("The sum is: ", sumNum);
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  const product = x * y;
+  return cb(product);
 }
+
+multiplyNums(2, 4, function(multiplyNum) {
+  console.log("The product is: ", multiplyNum);
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  const checkedArray = list.filter(li => {
+    return li === item;
+  });
+  return cb(checkedArray.length > 0 ? true : false);
 }
+
+contains("Pencil", items, function(isInside) {
+  console.log("The item is inside:", isInside);
+});
 
 /* STRETCH PROBLEM */
 
@@ -52,4 +81,17 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  let duplicateFree = [];
+  array.forEach(item => {
+    if (duplicateFree.includes(item)) {
+      return;
+    } else {
+      duplicateFree.push(item);
+    }
+  });
+  return cb(duplicateFree);
 }
+
+removeDuplicates(items, function(arr) {
+  console.log("Duplicate free array: ", arr);
+});
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,13 +1,20 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+function closureExample(testString) {
+  const insideParam = testString;
+  console.log(`I'm the inside parameter, ${insideParam}!`);
+}
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
-
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return function() {
+    count++;
+    return count;
+  };
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
@@ -18,4 +25,15 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  return (newObj = {
+    count: 0,
+    increment: function() {
+      this.count++;
+      return this.count;
+    },
+    decrement: function() {
+      this.count--;
+      return this.count;
+    }
+  });
 };
